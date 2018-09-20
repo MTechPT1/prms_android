b4f01435b36db9afbe6c71c23cd5f1e98c8f3d8d
wk <koh-waikin@hotmail.com>
2018-09-20 16:48:11 +0800

    Completed Create Schedule Use Case

--- app/src/main/java/sg/edu/nus/iss/phoenix/schedule/android/controller/MaintainScheduleController.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/schedule/android/controller/MaintainScheduleController.java
@@ -3,17 +3,24 @@ package sg.edu.nus.iss.phoenix.schedule.android.controller;
 import android.content.Intent;
 
 import sg.edu.nus.iss.phoenix.core.android.controller.MainController;
+import sg.edu.nus.iss.phoenix.schedule.android.delegate.CreateScheduleDelegate;
 import sg.edu.nus.iss.phoenix.schedule.android.delegate.DeleteScheduleDelegate;
 import sg.edu.nus.iss.phoenix.schedule.android.ui.ScheduleScreen;
 
 public class MaintainScheduleController {
 
+    CreateScheduleDelegate createDelegate;
     DeleteScheduleDelegate delDelegate;
 
     public void startUseCase(){
          Intent intent = new Intent(MainController.getApp(), ScheduleScreen.class);
          MainController.displayScreen(intent);
      }
+
+    public void createSchedule(){
+        createDelegate = new CreateScheduleDelegate(this);
+        //createDelegate.execute();
+    }
 
     public void deleteSchedule(){
         delDelegate = new DeleteScheduleDelegate(this);
