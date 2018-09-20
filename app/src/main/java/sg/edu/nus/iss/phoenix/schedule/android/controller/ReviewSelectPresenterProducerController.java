a78d25be18f297c7815d9cb050ae7395b01cc127
wk <koh-waikin@hotmail.com>
2018-09-20 15:30:30 +0800

    Completed logic for ReviewSelectPresenterProducer use case

--- app/src/main/java/sg/edu/nus/iss/phoenix/schedule/android/controller/ReviewSelectPresenterProducerController.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/schedule/android/controller/ReviewSelectPresenterProducerController.java
@@ -2,25 +2,29 @@ package sg.edu.nus.iss.phoenix.schedule.android.controller;
 
 import android.content.Intent;
 
+import sg.edu.nus.iss.phoenix.Constant;
 import sg.edu.nus.iss.phoenix.core.android.controller.MainController;
+import sg.edu.nus.iss.phoenix.schedule.android.delegate.RetrieveAllUsersDelegate;
+import sg.edu.nus.iss.phoenix.schedule.android.delegate.RetrieveUserDelegate;
 import sg.edu.nus.iss.phoenix.schedule.android.ui.ReviewSelectPresenterProducerScreen;
 
 public class ReviewSelectPresenterProducerController {
 
-     private int actionType;
 
-    public int getActionType() {
-        return actionType;
+    public void startUseCase(int role, String name) {
+        Intent intent = new Intent(MainController.getApp(), ReviewSelectPresenterProducerScreen.class);
+        intent.putExtra(Constant.USERNAME, name);
+        intent.putExtra(Constant.USERROLE, role);
+        MainController.displayScreen(intent);
     }
 
-    public void setActionType(int actionType) {
-        this.actionType = actionType;
+    public void retrieveAllUsers() {
+        RetrieveAllUsersDelegate retrieveAllUserDelegate = new RetrieveAllUsersDelegate();
+        //retrieveAllUserDelegate.execute();
     }
 
-    public void startUseCase(){
-         Intent intent = new Intent(MainController.getApp(), ReviewSelectPresenterProducerScreen.class);
-         MainController.displayScreen(intent);
-     }
-
-
+    public void retrieveUser(int userID) {
+        RetrieveUserDelegate retrieveUserDelegate = new RetrieveUserDelegate();
+        //retrieveUserDelegate.execute();
+    }
 }
