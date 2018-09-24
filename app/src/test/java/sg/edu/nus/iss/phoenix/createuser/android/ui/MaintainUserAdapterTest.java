cc939bc057c59bc84d8edc68d0410cf9f9c1e557
Changling <changling@singtel.com>
2018-09-24 14:37:48 +0800

    Revise MaintainUserAdapterTest

--- app/src/test/java/sg/edu/nus/iss/phoenix/createuser/android/ui/MaintainUserAdapterTest.java
+++ app/src/test/java/sg/edu/nus/iss/phoenix/createuser/android/ui/MaintainUserAdapterTest.java
@@ -20,6 +20,7 @@ public class MaintainUserAdapterTest {
         user.setProducer(false);
         user.setUserId("11223232");
         user.setUserName("ChangLing Liu");
+        user.setJoinDate("18 June 2018");
         maintainUserAdapter = new MaintainUserAdapter(new MaintainUserScreen(), user);
     }
 
@@ -31,19 +32,19 @@ public class MaintainUserAdapterTest {
 
     @Test
     public void getItemViewType() {
-        assertEquals(0, maintainUserAdapter.getItemViewType(0));
-        assertEquals(1, maintainUserAdapter.getItemViewType(1));
-        assertEquals(2, maintainUserAdapter.getItemViewType(2));
-        assertEquals(2, maintainUserAdapter.getItemViewType(3));
-        assertEquals(2, maintainUserAdapter.getItemViewType(10));
+        assertEquals("Current view type should be User Name(0)",0, maintainUserAdapter.getItemViewType(0));
+        assertEquals("Current view type should be Date (1)",1, maintainUserAdapter.getItemViewType(1));
+        assertEquals("Current view type should be Role (2)",2, maintainUserAdapter.getItemViewType(2));
+        assertEquals("Current view type should be Role (2)",2, maintainUserAdapter.getItemViewType(3));
+        assertEquals("Current view type should be Role (2)",2, maintainUserAdapter.getItemViewType(10));
     }
 
     @Test
     public void getItem() {
-       assertEquals(user.getUserName(),maintainUserAdapter.getItem(0));
-       assertEquals(user.getJoinDate(),maintainUserAdapter.getItem(1));
-       assertEquals("Presenter",maintainUserAdapter.getItem(2));
-       assertEquals("Producer", maintainUserAdapter.getItem(3));
+       assertEquals("Current User name should be ChangLing Liu",user.getUserName(),maintainUserAdapter.getItem(0));
+       assertEquals("Current User name should be 18 June 2018",user.getJoinDate(),maintainUserAdapter.getItem(1));
+       assertEquals("Current User name should be Presenter","Presenter",maintainUserAdapter.getItem(2));
+       assertEquals("Current User name should be Producer","Producer", maintainUserAdapter.getItem(3));
     }
 
     @Test
