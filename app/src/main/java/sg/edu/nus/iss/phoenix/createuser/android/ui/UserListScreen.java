5ac4bf3c854ee9c332173ff5ba1c776cf7d08b52
Changling <changling@singtel.com>
2018-09-24 22:35:59 +0800

    Add displayAllusers function

--- app/src/main/java/sg/edu/nus/iss/phoenix/createuser/android/ui/UserListScreen.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/createuser/android/ui/UserListScreen.java
@@ -16,7 +16,7 @@ public class UserListScreen extends AppCompatActivity {
 
     private ListView userListView;
     private UserListAdapter adapter;
-    private ArrayList<User> userList = new ArrayList<User>();
+    //private ArrayList<User> userList = new ArrayList<User>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +26,10 @@ public class UserListScreen extends AppCompatActivity {
     }
 
     private void initView(){
-         initDummyData();
+         //initDummyData();
          userListView = (ListView) findViewById(R.id.userlist);
-         adapter = new UserListAdapter(this,userList);
+         adapter = new UserListAdapter(this);
          userListView.setAdapter(adapter);
-
     }
 
     @Override
@@ -52,35 +51,40 @@ public class UserListScreen extends AppCompatActivity {
         }
     }
 
-    void initDummyData(){
-        User user1 = new User();
-        user1.setPresenter(true);
-        user1.setProducer(false);
-        user1.setUserId("11223232");
-        user1.setUserName("ChangLing Liu1");
-        userList.add(user1);
-
-        User user2 = new User();
-        user2.setPresenter(true);
-        user2.setProducer(false);
-        user2.setUserId("1122d2323232");
-        user2.setUserName("ChangLing Liu2");
-        userList.add(user2);
-
-        User user3 = new User();
-        user3.setPresenter(true);
-        user3.setProducer(false);
-        user3.setUserId("1123232");
-        user3.setUserName("ChangLing Liu3");
-        userList.add(user3);
-
-        User user4 = new User();
-        user4.setPresenter(true);
-        user4.setProducer(false);
-        user4.setUserId("112323232");
-        user4.setUserName("ChangLing Liu4");
-        userList.add(user4);
+    public void displayAllUsers(ArrayList<User> users){
+        adapter.setUserList(!users.isEmpty() ? users: new ArrayList<User>());
+        adapter.notifyDataSetChanged();
     }
+
+//    void initDummyData(){
+//        User user1 = new User();
+//        user1.setPresenter(true);
+//        user1.setProducer(false);
+//        user1.setUserId("11223232");
+//        user1.setUserName("ChangLing Liu1");
+//        userList.add(user1);
+//
+//        User user2 = new User();
+//        user2.setPresenter(true);
+//        user2.setProducer(false);
+//        user2.setUserId("1122d2323232");
+//        user2.setUserName("ChangLing Liu2");
+//        userList.add(user2);
+//
+//        User user3 = new User();
+//        user3.setPresenter(true);
+//        user3.setProducer(false);
+//        user3.setUserId("1123232");
+//        user3.setUserName("ChangLing Liu3");
+//        userList.add(user3);
+//
+//        User user4 = new User();
+//        user4.setPresenter(true);
+//        user4.setProducer(false);
+//        user4.setUserId("112323232");
+//        user4.setUserName("ChangLing Liu4");
+//        userList.add(user4);
+//    }
 
 }
 
